5a52bce4da79c09d8fc0a874e4d78856b4066b74
Alex Reinhart <areinhar@stat.cmu.edu>
2023-06-01 19:10:02 +0000

    version 0.5.0

--- inst/doc/multi-signals.R
+++ inst/doc/multi-signals.R
@@ -8,7 +8,7 @@ signals <- covidcast_signals(data_source = "usa-facts",
                              signal = c("confirmed_incidence_num",
                                         "deaths_incidence_num"),
                              start_day = start_day, end_day = end_day,
-                             geo_type = "state")
+                             geo_type = "state", geo_values = "tx")
 
 summary(signals[[1]])
 summary(signals[[2]])
@@ -20,37 +20,37 @@ aggregate_signals(signals) %>% head()
 
 ## -----------------------------------------------------------------------------
 aggregate_signals(signals, dt = c(-1, 0)) %>%
-  filter(geo_value == "tx") %>% head()
+  head()
 aggregate_signals(signals, dt = list(0, c(-1, 0, 1))) %>%
-  filter(geo_value == "tx") %>% head()
+  head()
 
 ## -----------------------------------------------------------------------------
 aggregate_signals(signals[[1]], dt = c(-1, 0, 1)) %>%
-  filter(geo_value == "tx") %>% head()
+  head()
 
 ## -----------------------------------------------------------------------------
 aggregate_signals(signals, format = "long") %>%
-  filter(geo_value == "tx") %>% head()
+  head()
 
 aggregate_signals(signals, dt = c(-1, 0), format = "long") %>%
-  filter(geo_value == "tx") %>% head()
+  head()
 
 aggregate_signals(signals, dt = list(-1, 0), format = "long") %>%
-  filter(geo_value == "tx") %>% head()
+  head()
 
 ## -----------------------------------------------------------------------------
 aggregate_signals(signals[[1]], dt = c(-1, 0), format = "long") %>%
-  filter(geo_value == "tx") %>% head()
+  head()
 
 ## -----------------------------------------------------------------------------
 aggregate_signals(signals, dt = list(-1, 0)) %>%
   covidcast_longer() %>%
-  filter(geo_value == "tx") %>% head()
+  head()
 
 ## -----------------------------------------------------------------------------
 aggregate_signals(signals, dt = list(-1, 0), format = "long") %>%
   covidcast_wider() %>%
-  filter(geo_value == "tx") %>% head()
+  head()
 
 ## -----------------------------------------------------------------------------
 df_cor1 <- covidcast_cor(x = aggregate_signals(signals[[1]], dt = -7,
